0f4adc42650a5c065bfef2a4b65e2ad4c091eab0
ibs-tac <eslam.abdelgawad>
2023-09-11 14:07:17 +0300

    uploud bmc report  on staging v2

--- ibs_bmc_invoice_report/models/sale_order.py
+++ ibs_bmc_invoice_report/models/sale_order.py
@@ -9,3 +9,12 @@ class SaleOrder(models.Model):
 
     sales_executive = fields.Char()
     sales_manager = fields.Char(default="Ahmed Aladly")
+
+
+
+class SaleOrderLine(models.Model):
+    """ inherit Sale Order Line """
+    _inherit = 'sale.order.line'
+
+    pregnancy = fields.Char()
+    weight = fields.Char()
