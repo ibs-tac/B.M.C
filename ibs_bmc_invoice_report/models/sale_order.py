015217a98331765de3bd376959b6617b20cc6876
ibs-tac <eslam.abdelgawad>
2024-01-03 17:56:59 +0200

    uploud ibs_bmc_invoice_report addons  on production v3

--- ibs_bmc_invoice_report/models/sale_order.py
+++ ibs_bmc_invoice_report/models/sale_order.py
@@ -1,20 +1,30 @@
 # -*- coding: utf-8 -*-
 """ Sale Order """
-from odoo import api, fields, models, _
-from odoo.exceptions import UserError, Warning, ValidationError
+from odoo import fields, models
+
 
 class SaleOrder(models.Model):
     """ inherit Sale Order """
     _inherit = 'sale.order'
 
-    sales_executive = fields.Char()
+    sales_executive_id = fields.Many2one(
+        'res.users'
+    )
     sales_manager = fields.Char(default="Ahmed Aladly")
 
+    def send_whatsapp(self):
+        link = "https://web.whatsapp.com"
+        return {
+            'type': 'ir.actions.act_url',
+            'url': link,
+            'target': 'new',
+            'res_id': self.id,
+        }
 
 
 class SaleOrderLine(models.Model):
     """ inherit Sale Order Line """
     _inherit = 'sale.order.line'
 
-    pregnancy = fields.Char()
+    load = fields.Char()
     weight = fields.Char(string="Color")
--- ibs_mobile_unique/models/res_partner.py
+++ ibs_mobile_unique/models/res_partner.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """ Res Partner """
-from odoo import api, fields, models, _
-from odoo.exceptions import UserError, Warning, ValidationError
+from odoo import _, api, models
+from odoo.exceptions import ValidationError
 
 
 class ResPartner(models.Model):
@@ -11,9 +11,23 @@ class ResPartner(models.Model):
     @api.constrains('mobile')
     def _check_mobile_num(self):
         """ Validate mobile_num """
-        if self.mobile:
-            mobile = self.env['res.partner'].search(
-                [('mobile', '=', self.mobile), ('id', '!=', self.id)], limit=1)
-            if mobile:
+        for rec in self:
+            if rec.mobile:
+                record = self.env['res.partner'].search(
+                    [('mobile', '=', rec.mobile), ('id', '!=', rec.id)],
+                    limit=1)
+                if record:
+                    raise ValidationError(
+                        _(
+                            'This mobile number  %s is with one of the clients' % rec.mobile))
+
+    @api.constrains('name')
+    def _check_name(self):
+        """ Validate name """
+        for rec in self:
+            record = self.env['res.partner'].search(
+                [('name', '=', rec.name), ('id', '!=', rec.id)], limit=1)
+            if record:
                 raise ValidationError(
-                    _('This mobile number  %s is with one of the clients' % self.mobile))
+                    _(
+                        'Name Must be Unique'))
